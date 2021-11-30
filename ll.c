fd966080dcaf1385921caa4ce8b1f6ea77ce0e51
Abhishek <94548108+ignabhishek@users.noreply.github.com>
2021-11-30 11:34:46 +0530

    Update ll.c

--- ll.c
+++ ll.c
@@ -9,6 +9,7 @@ struct node
 
 struct node *start=NULL;
 struct node *create(struct node *);
+struct node *insert(struct node *);
 struct node *display(struct node *);
 
 int main()
@@ -16,7 +17,7 @@ int main()
 	int option;
 	do
 	{
-		printf("\n1.Create a list \n2.Display \n3.Exit \n");
+		printf("\n1.Create a list \n2.Insertion \n3.Display \n4.Exit \n");
 		printf("Enter your option: ");
 		scanf("%d",&option);
 		printf("\n");
@@ -24,21 +25,81 @@ int main()
 		{
 			case 1: {
 					start=create(start);
-					printf("\n\nLinked List Created");
+					printf("\nLinked List Created\n");
 					break;
 				}
 			case 2: {
+					start=insert(start);
+					break;
+				}
+			case 3: {
 					start=display(start);
 					printf("\n");
 					break;
 				}
+			case 4: {
+					printf("Exiting\n");
+					break;
+				}
 			default: {
-					printf("\n\nExiting");
+					printf("\nInvalid choice\n");
 					break;
 				 }
 		}
-	}while(option!=3);
+	}while(option!=4);
 }
+
+
+struct node *insert(struct node *start)
+{
+	struct node *newnode,*ptr,*preptr,*temp;
+	int num,ch;
+	if(start==NULL)
+	{
+		printf("\nPlease Create a list\n");
+	}
+	else
+	{
+		printf("Enter the data:");
+		scanf("%d",&num);
+		newnode=(struct node *)malloc(sizeof(struct node));
+		newnode->data=num;
+		printf("\n1.Insert at the Beginning \n2.Insert at End \n3.Insert at the Middle\n");
+		printf("Enter your choice: ");
+		scanf("%d",&ch);
+		switch(ch)
+		{	
+			case 1: {
+					newnode->next=start;
+					start=newnode;
+					printf("\nInserted at the Beginning\n");
+					break;
+				}
+			case 2: {	
+					ptr=start;
+					while(ptr->next!=NULL)
+					{
+						ptr=ptr->next;
+					}
+					ptr->next=newnode;
+					newnode->next=NULL;
+					printf("\nInserted at the End\n");
+					break;
+				}
+			case 3: {
+					printf("\nInserted at the Middle\n");
+					break;
+				}
+			default: {
+				 	printf("\nInvalid choice\n");
+					break;
+				 }
+		
+		
+		}
+	}
+}
+
 
 struct node *create(struct node *start)
 {	
@@ -72,6 +133,7 @@ struct node *create(struct node *start)
 	return start;
 }
 
+
 struct node *display(struct node *start)
 {
 	struct node *ptr;
@@ -84,4 +146,5 @@ struct node *display(struct node *start)
 	printf("NULL");
 	return start;
 }
+
 
